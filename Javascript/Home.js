ee67171e9a38d10a6c25a9adf4e0c8bd7aea6bed
Manno <si.jorgelucas@gmail.com>
2022-03-24 16:52:21 -0300

    Initial commit

---
+++ Javascript/Home.js
@@ -0,0 +1,10 @@
+function mostrarValor(obj) {
+    obj.innerHTML = "<b>R$10,00</b>"
+}
+function voltarValor(obj) {
+    obj.innerHTML = "<b>Bolo decorado</b>";
+}
+function lightmode() {
+}
+function darkmode() {
+}
